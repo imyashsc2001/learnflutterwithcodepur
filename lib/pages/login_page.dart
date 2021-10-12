140320cd778fe37b31b1469fb034abfe8e24f678
Yash Chavan <imyashsc.2001@gmail.com>
2021-10-12 16:00:22 +0530

    day7 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +28,7 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(height: 16),
               Text(
                 email.isEmpty ? 'Welcome' : 'Welcome $email',
-                style: TextStyle(
+                style: const TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 24,
                 ),
@@ -38,57 +39,65 @@ class _LoginPageState extends State<LoginPage> {
                   vertical: 8,
                   horizontal: 32,
                 ),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          email = value;
-                        });
-                      },
-                      decoration: const InputDecoration(
-                        labelText: 'Email',
-                        hintText: 'Enter Your Email',
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        onChanged: (value) {
+                          setState(() {
+                            email = value;
+                          });
+                        },
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter Your Email',
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Email cannot be empty.";
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: 'Password',
-                        hintText: 'Enter Password',
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter Password',
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password cannot be empty.";
+                          } else if (value.length < 8) {
+                            return "Password should have 8 or more characters.";
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    InkWell(
-                      onTap: () async {
-                        setState(() {
-                          //!changeButton;
-                          changeButton
-                              ? changeButton = false
-                              : changeButton = true;
-                        });
-                        await Future.delayed(const Duration(milliseconds: 210));
-                        Navigator.pushNamed(context, MyRoutes.routeHome);
-                      },
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 200),
-                        width: changeButton ? 1 : 150,
-                        height: 50,
-                        alignment: Alignment.center,
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
+                      const SizedBox(height: 16),
+                      Material(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(16),
+                        child: InkWell(
+                          onTap: onClickLogin,
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 200),
+                            width: changeButton ? 1 : 150,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
                           ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               )
             ],
@@ -96,5 +105,15 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+
+  onClickLogin() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton ? changeButton = false : changeButton = true;
+      });
+      await Future.delayed(const Duration(milliseconds: 210));
+      Navigator.pushNamed(context, MyRoutes.routeHome);
+    }
   }
 }
